d63a672986399adfdfe80e28efab60cea49b3fdf
Rich Barnes <locklear904@gmail.com>
2023-04-04 14:47:09 -0400

    Create playRound function
    Created a function that when run, takes a prompt input from the user
    to run against the computerSelection function. It then spits out
    a string based on the results

--- javascript.js
+++ javascript.js
@@ -1,13 +1,41 @@
 
+
 function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * (4-1) + 1); //assign random number 1-3 to variable
-    if (computerChoice === 1) { //if number is certain number - assign rock paper or scissors string to variable
-        computerChoice = "Rock";
+    //assign random number 1-3 to variable//assign random number 1-3 to variable
+    let computerChoice = Math.floor(Math.random() * (4-1) + 1);
+    //assign rock paper or scissors to the random number
+    if (computerChoice === 1) { 
+        computerChoice = "rock";
     }   else if (computerChoice === 2) {
-        computerChoice = "Paper";
+        computerChoice = "paper";
     }   else {
-        computerChoice = "Scissors";
+        computerChoice = "scissors";
     }
-    console.log(computerChoice);
-    //return computerChoice;//return the string
+    return computerChoice;
 }
+
+let playerChoice = prompt("Please enter rock, paper, or scissors", "");
+let computerChoice = getComputerChoice();
+
+function playRound(playerChoice, computerChoice) {
+    playerChoice.toLowerCase();
+    if (playerChoice === computerChoice) {
+        console.log("You tied!");
+    }   else if (playerChoice === "rock" & computerChoice === "paper") {
+        console.log("Paper beats rock, you lose!");
+    }   else if (playerChoice === "rock" & computerChoice === "scissors") {
+        console.log("Rock beats scissors, you win!");
+    }   else if (playerChoice === "paper" & computerChoice === "rock") {
+        console.log("Paper beats rock, you win!");
+    }   else if (playerChoice === "paper" & computerChoice === "scissors") {
+        console.log("Scissors beats paper, you lose!");
+    }   else if (playerChoice === "scissors" & computerChoice === "rock") {
+        console.log("Rock beats scissors, you lose!");
+    }   else if (playerChoice === "scissors" & computerChoice === "paper") {
+        console.log("Scissors beats paper, you win!");
+    }   else {
+        console.log("Please try again!")
+    }
+}
+
+playRound(playerChoice, computerChoice);
