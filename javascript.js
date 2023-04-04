65efd061750b7ef5e82ca2adafa3e7fecd4d61e1
Rich Barnes <locklear904@gmail.com>
2023-04-04 15:36:40 -0400

    Write game function
    Wrote the basic game function that calls the playRound function
    five times and logs the result. Added a playerScore variable that
    gets added or subtracted to depending on the result of each round.
    Had to increment/decrement using a prefix for each possible result.

--- javascript.js
+++ javascript.js
@@ -14,28 +14,56 @@ function getComputerChoice() {
     return computerChoice;
 }
 
-let playerChoice = prompt("Please enter rock, paper, or scissors", "");
+let playerChoice = " ";
 let computerChoice = getComputerChoice();
+let playerScore = 0;
 
 function playRound(playerChoice, computerChoice) {
+    playerChoice = prompt("Please enter rock, paper, or scissors", "");
     playerChoice.toLowerCase();
     if (playerChoice === computerChoice) {
         console.log("You tied!");
     }   else if (playerChoice === "rock" & computerChoice === "paper") {
         console.log("Paper beats rock, you lose!");
+        return --playerScore;
     }   else if (playerChoice === "rock" & computerChoice === "scissors") {
         console.log("Rock beats scissors, you win!");
+        return ++playerScore;
     }   else if (playerChoice === "paper" & computerChoice === "rock") {
         console.log("Paper beats rock, you win!");
+        return ++playerScore;
     }   else if (playerChoice === "paper" & computerChoice === "scissors") {
         console.log("Scissors beats paper, you lose!");
+        return --playerScore;
     }   else if (playerChoice === "scissors" & computerChoice === "rock") {
         console.log("Rock beats scissors, you lose!");
+        return --playerScore;
     }   else if (playerChoice === "scissors" & computerChoice === "paper") {
         console.log("Scissors beats paper, you win!");
+        return ++playerScore;
     }   else {
         console.log("Please try again!")
     }
 }
 
-playRound(playerChoice, computerChoice);
+function game() {
+    playRound(playerChoice, computerChoice);
+    console.log(playerScore);
+    playRound(playerChoice, computerChoice);
+    console.log(playerScore);
+    playRound(playerChoice, computerChoice);
+    console.log(playerScore);
+    playRound(playerChoice, computerChoice);
+    console.log(playerScore);
+    playRound(playerChoice, computerChoice);
+    console.log(playerScore);
+    if (playerScore > 0) {
+        console.log("You win the game!");
+    }   else if (playerScore < 0) {
+        console.log("You lose the game!");
+    }   else {
+        console.log("You tied the game!");
+    }
+}
+
+game();
